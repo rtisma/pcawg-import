f3d5b96e45df92be0bb3ef5367c9374cf0b26209
Robert Tisma <robert.tisma@oicr.on.ca>
2017-03-07 15:54:50 -0500

    removed unused converters

--- pcawg-import-client/src/main/java/org/icgc/dcc/pcawg/client/core/model/metadata/converters/FileMetaDataContextConverter.java
+++
@@ -1,9 +0,0 @@
-package org.icgc.dcc.pcawg.client.core.model.metadata.converters;
-
-import org.icgc.dcc.pcawg.client.core.model.metadata.FileMetaDataContext;
-
-public interface FileMetaDataContextConverter<T> {
-
-  T convertFromFileMetaDataContext(FileMetaDataContext fileMetaDataContext);
-
-}
