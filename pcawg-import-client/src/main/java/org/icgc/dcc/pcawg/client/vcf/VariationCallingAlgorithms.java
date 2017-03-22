d3df68ef6d8b40c8aaf2f361e9eb72dce12d8f7d
Robert Tisma <robert.tisma@oicr.on.ca>
2017-03-22 09:37:56 -0400

    added VariantioCallingAlgorithms enum which has requieed logic for creating text

--- pcawg-import-client/src/main/java/org/icgc/dcc/pcawg/client/vcf/VariationCallingAlgorithms.java
+++ pcawg-import-client/src/main/java/org/icgc/dcc/pcawg/client/vcf/VariationCallingAlgorithms.java
@@ -23,13 +23,14 @@ import static org.icgc.dcc.pcawg.client.vcf.WorkflowTypes.SANGER;
  */
 public enum VariationCallingAlgorithms {
 
-
   CONSENSUS_PCAWG_SNV_MNV("PCAWG Consensus SNV-MNV caller", CONSENSUS, set(SNV_MNV)),
   CONSENSUS_PCAWG_INDEL("PCAWG Consensus INDEL caller", CONSENSUS, set(INDEL)),
   SANGER_PIPELINE("Sanger variant call pipeline", SANGER, set(SNV_MNV, INDEL)),
   DKFZ_EMBL_PIPELINE("DKFS/EMBL variant call pipeline", DKFZ_EMBL, set(SNV_MNV, INDEL)),
   MUSE_PIPELINE("Muse variant call pipeline", MUSE, set(SNV_MNV, INDEL)),
   BROAD_PIPELINE("Broad variant call pipeline", BROAD, set(SNV_MNV, INDEL));
+
+  private static final String CLASS_NAME = VariationCallingAlgorithms.class.getName();
 
   VariationCallingAlgorithms(@NonNull String text, final WorkflowTypes workflowType, @NonNull Set<DataTypes> dataTypes) {
     dataTypes.forEach(x -> add(workflowType, x, text));
@@ -46,13 +47,17 @@ public enum VariationCallingAlgorithms {
       workflowMap.put(workflowType, newEnumMap(DataTypes.class));
     }
     val dataTypeMap = workflowMap.get(workflowType);
+    checkArgument(!dataTypeMap.containsKey(dataType),
+        "The dataType [%s] is already defined for the workflowType [%s] in %s", dataType,workflowType, CLASS_NAME);
     dataTypeMap.put(dataType, text);
   }
 
   public static String get(WorkflowTypes workflowType, DataTypes dataType){
-    checkArgument(workflowMap.containsKey(workflowType), "The workflowType [%s] is not defined in %s",workflowType, VariationCallingAlgorithms.class.getName());
+    checkArgument(workflowMap.containsKey(workflowType),
+        "The workflowType [%s] is not defined in %s",workflowType, CLASS_NAME);
     val dataTypeMap = workflowMap.get(workflowType);
-    checkArgument(dataTypeMap.containsKey(dataType), "The dataType [%s] is not defined for workflowType [%s] in %s", dataType, workflowType, VariationCallingAlgorithms.class.getName());
+    checkArgument(dataTypeMap.containsKey(dataType),
+        "The dataType [%s] is not defined for workflowType [%s] in %s", dataType, workflowType, CLASS_NAME);
     return dataTypeMap.get(dataType);
   }
 
